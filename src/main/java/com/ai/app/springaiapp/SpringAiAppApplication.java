42c72d63afde2fd06931c4eb6a3216b28cac1d25
zhangc <1277051604@qq.com>
2025-04-06 20:53:57 +0800

    fix:基础maven包

---
+++ src/main/java/com/ai/app/springaiapp/SpringAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.ai.app.springaiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ai/app/springaiapp/SpringAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.app.springaiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
